931c3a1c2f1e8d427ba9485f9d175295c679cbfa
ijgnd <fooijgnd@barijgnd>
2020-07-29 22:13:30 +0200

    update gc

--- src/add_hyperlinks.py
+++ src/add_hyperlinks.py
@@ -26,7 +26,11 @@ addon_path = os.path.dirname(__file__)
 
 
 def gc(arg, fail=False):
-    return mw.addonManager.getConfig(__name__).get(arg, fail)
+    conf = mw.addonManager.getConfig(__name__)
+    if conf:
+        return conf.get(arg, fail)
+    else:
+        return fail
 
 
 def hlunlink(editor):
