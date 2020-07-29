04758ceea87d22b192913f89d086cf188eca5c9e
ijgnd <fooijgnd@barijgnd>
2020-07-29 22:35:28 +0200

    encode_illegal_characters_in_links was much to aggressive

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -27,19 +27,19 @@ def escape_html_chars(s):
 def some_percent_encoding(s):
     # https://en.wikipedia.org/wiki/Help:URL#Fixing_links_with_unsupported_characters
     replacements = {
-    " ": "%20",
+    # " ": "%20",
     '"': "%22",
-    "'": "%27",
-    ",": "%2C",
-    ";": "%3B",
-    "<": "%3C",
-    ">": "%3E",
-    "?": "%3F",
-    "[": "%5B",
-    "]": "%5D",
-    "{": "%7B",
-    "|": "%7C",
-    "}": "%7D",
+    # "'": "%27",
+    # ",": "%2C",
+    # ";": "%3B",
+    # "<": "%3C",
+    # ">": "%3E",
+    # "?": "%3F",
+    # "[": "%5B",
+    # "]": "%5D",
+    # "{": "%7B",
+    # "|": "%7C",
+    # "}": "%7D",
     }
     result = "".join(replacements.get(c, c) for c in s)
     return result
